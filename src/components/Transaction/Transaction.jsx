e02b921618f542e40b7d868ccce33a5bc874da55
Grandmaster M <mitchel.stroosnijder@gmail.com>
2020-04-19 00:06:27 +0200

    feat: add sign logic based on transaction amount

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
-export default function Transaction(props) {
+export default function Transaction({ transaction }) {
+    const [sign, setSign] = useState('')
+
+    useEffect(() => {
+        if (transaction.amount > 0) {
+            return setSign('+')
+        } else if (transaction.amount < 0) {
+            return setSign('-')
+        } else {
+            return setSign('')
+        }
+    }, [transaction.amount])
+
     return (
         <div>
             <li className='minus'>
-                {props.transaction.text}
-                <span>{props.transaction.amount}</span>
+                {transaction.text}
+                <span>{sign}${Math.abs(transaction.amount)}</span>
                 <button className="delete-btn">x</button>
             </li>
         </div>
