f2fd86e12df20a7b3561dee6aa1df81e632fc40f
Grandmaster M <mitchel.stroosnijder@gmail.com>
2020-04-19 05:50:15 +0200

    Updates

---
+++ precache-manifest.233f5c4921a3cc4edc406224334b5557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6f52587cd6dbb7b849f0f99bc70440",
+    "url": "/financial-tracker/index.html"
+  },
+  {
+    "revision": "4a9834d51b8a24ccd2b9",
+    "url": "/financial-tracker/static/css/main.8691e19d.chunk.css"
+  },
+  {
+    "revision": "67f06b310044aec25c34",
+    "url": "/financial-tracker/static/js/2.4514890f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/financial-tracker/static/js/2.4514890f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9834d51b8a24ccd2b9",
+    "url": "/financial-tracker/static/js/main.bdbf9e10.chunk.js"
+  },
+  {
+    "revision": "29d83793bfc0ce868118",
+    "url": "/financial-tracker/static/js/runtime-main.3a36f37b.js"
+  }
+]);
